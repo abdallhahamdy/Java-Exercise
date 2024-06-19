679da7f1e47f20fc3e37341658ab9f1960b0ca63
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-19 23:08:51 +0300

    Exercise_5

---
+++ Java Basic I/Exercise_5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercise_5 {
+
+    static int x = 25;
+    static int y = 5;
+
+    public static void main(String[] args) {
+
+        // Create a Scanner object to read input from the user
+        // Scanner in = new Scanner(System.in);
+
+        // Prompt the user to input the first number
+        // System.out.print("Input first number: ");
+
+        // Read and store the first number
+        // int num1 = in.nextInt();
+
+        // Prompt the user to input the second number
+        // System.out.println("Input second number");
+
+        // Read and store the second number
+        // int num2 = in.nextInt();
+
+        // Calculate the product of the two numbers and display the result
+        // System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+
+
+        // Calculate and print the product of x and y
+        System.out.println(x * y);
+
+
+    }
+}
