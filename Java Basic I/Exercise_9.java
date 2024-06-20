8af89e62d69ea8fba53c8930e255af4417287eb4
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-20 21:25:13 +0300

    Exercise_9

---
+++ Java Basic I/Exercise_9.java
@@ -0,0 +1,8 @@
+public class Exercise_9 {
+
+    public static void main(String[] args) {
+
+        // Calculate and print the result of a mathematical expression
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
