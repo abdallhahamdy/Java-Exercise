ac9bdff064ee6c898d2096c680ad9eaf4fa8f64c
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-28 09:24:15 +0300

    Exercise_5 OOP Inheritance

---
+++ OOP/Inheritance/Exercise_5/SavingsAccount.java
@@ -0,0 +1,17 @@
+package Inheritance.Exercise_5;
+
+// Child class SavingsAccount
+public class SavingsAccount extends BankAccount {
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (getBalance() - amount < 100) {
+            System.out.println("Minimum balance of $100 required!");
+        } else {
+            super.withdraw(amount);
+        }
+    }
+}
