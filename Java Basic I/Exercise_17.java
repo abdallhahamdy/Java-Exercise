84f90dd429dba071c284fe0ae65ee95886fdd405
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-28 06:16:31 +0300

    Exercise_17

---
+++ Java Basic I/Exercise_17.java
@@ -0,0 +1,26 @@
+public class Exercise_17 {
+    public static void main(String[] args) {
+
+        // Display a pattern to create an ASCII art representation of a simple face
+        // System.out.println(" +\"\"\"\"\"+ ");
+        // System.out.println("[| o o |]");
+        // System.out.println(" | ^   | ");
+        // System.out.println(" | '-' | ");
+        // System.out.println(" +-----+ ");
+
+        // Create an array to store lines of an ASCII art representation
+        String[] arra = new String[5];
+
+        // Populated the array with lines to form an ASCII art representation of a simple face
+        arra[0] = " +\"\"\"\"\"+ ";
+        arra[1] = "[| o o |]";
+        arra[2] = " |  ^  |";
+        arra[3] = " | '-' |";
+        arra[4] = " +-----+";
+
+        // Use a loop to print each line of the ASCII art representation
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arra[i]);
+        }
+    }
+}
