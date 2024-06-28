1b70d2ae1527c9821fa7107c3f45153148f40e7d
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-28 06:54:05 +0300

    Exercise_1 OOP Inheritance

---
+++ OOP/Inheritance/Exercise_1/Cat.java
@@ -0,0 +1,8 @@
+package Inheritance.Exercise_1;
+
+public class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("The cat quarrels.");
+    }
+}
---
+++ OOP/Inheritance/Exercise_1/Main.java
@@ -0,0 +1,10 @@
+package Inheritance.Exercise_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Cat cat = new Cat();
+        animal.makeSound();
+        cat.makeSound();
+    }
+}
