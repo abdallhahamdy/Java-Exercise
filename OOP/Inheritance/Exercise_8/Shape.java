e0b3a8d34e0d4aa7358e07fa5d350de912d2e345
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-29 00:18:54 +0300

    OOP Inheritance Exercise_8

---
+++ OOP/Inheritance/Exercise_8/Shape.java
@@ -0,0 +1,12 @@
+package Inheritance.Exercise_8;
+
+// Parent class Shape
+public class Shape {
+    public double getPerimeter() {
+        return 0.0;
+    }
+
+    public double getArea() {
+        return 0.0;
+    }
+}
