f3a03559f4ee08812d7d6adfe02bd07bf8586703
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-28 07:05:04 +0300

    Exercise_3 OOP Inheritance

---
+++ OOP/Inheritance/Exercise_3/Main.java
@@ -0,0 +1,10 @@
+package Inheritance.Exercise_3;
+
+// Main Class
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(3.0, 10.0);
+        double area = rectangle.getArea();
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
---
+++ OOP/Inheritance/Exercise_3/Shape.java
@@ -0,0 +1,8 @@
+package Inheritance.Exercise_3;
+
+// Parent Class Shape
+public class Shape {
+    public double getArea() {
+        return 0.0;
+    }
+}
