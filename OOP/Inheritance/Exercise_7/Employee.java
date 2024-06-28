5125672c161cff6efac939308e7923048b83152d
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-29 00:18:41 +0300

    OOP Inheritance Exercise_7

---
+++ OOP/Inheritance/Exercise_7/Employee.java
@@ -0,0 +1,22 @@
+package Inheritance.Exercise_7;
+
+// Child class Employee
+public class Employee extends Person {
+    private int employeeId;
+    private String jobTitle;
+
+    public Employee(String firstName, String lastName, int employeeId, String jobTitle) {
+        super(firstName, lastName);
+        this.employeeId = employeeId;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName() + ", " + jobTitle;
+    }
+}
