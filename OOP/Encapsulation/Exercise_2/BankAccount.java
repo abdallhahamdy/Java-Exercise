680b93b946b52e850d404cad07790d298f12fba3
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-30 18:30:56 +0300

    Exercise_2 OOP Encapsulation

---
+++ OOP/Encapsulation/Exercise_2/BankAccount.java
@@ -0,0 +1,25 @@
+package Encapsulation.Exercise_2;
+
+// BankAccount.java
+// BankAccount Class
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
