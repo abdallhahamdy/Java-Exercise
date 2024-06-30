08fbed4fb91030292c5236ad109fee2efd4c48a5
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-30 18:50:44 +0300

    Exercise_5 OOP Encapsulation

---
+++ OOP/Encapsulation/Exercise_5/Circle.java
@@ -0,0 +1,23 @@
+package Encapsulation.Exercise_5;
+// Circle.java
+// Circle Class
+
+public class Circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
