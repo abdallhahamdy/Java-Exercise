0fe4285e9e536a4cef1a8146f257db81d27a3d59
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-07-13 16:48:04 +0300

    OOP Exercise_5

---
+++ OOP/ObjectOrientedProgramming/Exercise_4/Circle.java
@@ -0,0 +1,19 @@
+package ObjectOrientedProgramming.Exercise_4;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+
+}
