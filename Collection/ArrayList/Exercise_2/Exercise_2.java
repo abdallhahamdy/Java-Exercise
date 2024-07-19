c424c5f06f81401dcfb4dee2aa7dcac347ced41a
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-07-19 14:26:21 +0300

    Exercise_2 ArrayList

---
+++ Collection/ArrayList/Exercise_2/Exercise_2.java
@@ -0,0 +1,20 @@
+package Exercise_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        List<String> list_Strings = new ArrayList<String>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        // Print the list
+        for (String element : list_Strings) {
+            System.out.println(element);
+        }
+
+    }
+}
