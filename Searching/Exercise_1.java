f95c12fa58041b825c5b086a869fe4d1744560bc
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-04 03:25:28 +0300

    Searching Exercise_1

--- Searching/Exercise_1.java
+++ Searching/Exercise_1.java
@@ -2,15 +2,47 @@ public class Exercise_1 {
     public static int binarySearch(int[] nums, int flag) {
         int hi_num = nums.length - 1;
         int lo_num = 0;
+
+        System.out.println("hi_num: " + hi_num);
+        System.out.println("lo_num: " + lo_num);
+        System.out.println("-------------------");
+
         while (hi_num >= lo_num) {
+
+            System.out.println("hi_num: " + hi_num);
+            System.out.println("lo_num: " + lo_num);
+            System.out.println("-------------------");
+
             int guess = (lo_num + hi_num) >>> 1;
+
+            System.out.println("guess: " + guess);
+            System.out.println("-------------------");
+
+            System.out.println("flag: " + flag);
+            System.out.println("-------------------");
+
+            System.out.println("nums[guess]: " + nums[guess]);
+            System.out.println("-------------------");
+
+
             if (nums[guess] > flag) {
                 hi_num = guess - 1;
+                System.out.println("hi_num inside condition: " + hi_num);
+                System.out.println("-------------------");
+
             } else if (nums[guess] < flag) {
                 lo_num = guess + 1;
+                System.out.println("lo_num inside condition: " + lo_num);
+                System.out.println("-------------------");
+
             } else {
+                System.out.println("guess inside condition : " + guess);
+                System.out.println("-------------------");
+
                 return guess;
             }
+
+            System.out.println("-------------------");
         }
 
         return -1;
