147630641c2cfd6e51cd111ad16af78004f4f7bc
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-08 13:41:47 +0300

    LinkedList Exercise_1

---
+++ Collection/LinkedList/Exercise_1.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class Exercise_1 {
+    public static void main(String[] args) {
+        LinkedList<String> l_list = new LinkedList<>();
+        l_list.add("Red");
+        l_list.add("Green");
+        l_list.add("Black");
+        l_list.add("White");
+        l_list.add("Pink");
+        l_list.add("Yellow");
+
+        System.out.println("The linked list: " + l_list);
+    }
+}
