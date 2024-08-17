e038d8b667bfe7ddb406005978846acda6b764bd
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-17 04:07:01 +0300

    Exercise TreeSet_7

---
+++ Collection/TreeSet/TreeSet_7.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+public class TreeSet_7 {
+    public static void main(String[] args) {
+        // create an empty tree set
+        TreeSet<String> t_set = new TreeSet<String>();
+        // use add() method to add values in the tree set
+        t_set.add("Red");
+        t_set.add("Green");
+        t_set.add("Black");
+        t_set.add("Pink");
+        t_set.add("orange");
+        System.out.println("Original tree set: " + t_set);
+        System.out.println("Size of the tree set: " + t_set.size());
+    }
+}
