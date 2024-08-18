3e86f871e4393c0b43fdaa3ad62949fdb83e8bde
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-18 03:04:10 +0300

    Exercise TreeSet_8

---
+++ Collection/TreeSet/TreeSet_8.java
@@ -0,0 +1,30 @@
+import java.util.TreeSet;
+
+public class TreeSet_8 {
+    public static void main(String[] args) {
+
+        // create an empty tree set
+        TreeSet<String> t_set1 = new TreeSet<String>();
+        // use add() method to add values in the tree set
+        t_set1.add("Red");
+        t_set1.add("Green");
+        t_set1.add("Black");
+        t_set1.add("Pink");
+        t_set1.add("orange");
+
+        System.out.println("Free Tree set: "+t_set1);
+
+        TreeSet<String> t_set2 = new TreeSet<String>();
+        t_set2.add("Red");
+        t_set2.add("Pink");
+        t_set2.add("Black");
+        t_set2.add("Orange");
+        System.out.println("Second Tree set: " + t_set2);
+
+        // Comparison output in tree set
+        TreeSet<String> new_t_set = new TreeSet<String>();
+        for (String element : t_set1) {
+            System.out.println(t_set2.contains(element) ? "Yes" : "No");
+        }
+    }
+}
